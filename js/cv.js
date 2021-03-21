44608bd02b2ab3821a9431102142b3861bf5b429
KommuSoft <3482343+KommuSoft@users.noreply.github.com>
2021-03-21 19:36:20 +0000

    Deploying to gh-pages from @ hapytexU/curriculum-vitae@0b866ea3628e2898e45e48336edc4303885da563 🚀

---
+++ js/cv.js
@@ -0,0 +1,111 @@
+var Curriculum = (function() {
+	var $container = $( '#cv-leaflet' ),
+		$cover = $container.find( 'div.cv-cover' ),
+    $coverclick = $cover.find('.cv-overlay'),
+    $coverfront = $cover.find('.cv-front'),
+    $covercontent = $coverfront.find('.cv-content'),
+		$middle = $container.find( 'div.cv-middle' ),
+		$right = $container.find( 'div.cv-right' ),
+		$open = $cover.find('a.cv-button-open'),
+		$close = $right.find('span.cv-close'),
+		$details = $container.find( 'a.cv-viewdetails' ),
+    state = false,
+
+		init = function() {
+
+			initEvents();
+
+		},
+		initEvents = function() {
+			$covercontent.on( 'click', function( event ) {
+        var target = event.target ? event.target : event.srcElement;
+        if(target.id == 'cv-frontopen') {
+          if($container.hasClass('cv-open')) {
+            closeMenu();
+          } else {
+            openMenu();
+          }
+  				return false;
+        }
+			} );
+
+			$right.on( 'click', function( event ) {
+        if($container.hasClass('cv-flip')) {
+          unflipMenu();
+        } else {
+  				flipMenu();
+        }
+				return false;
+
+			} );
+
+
+			$close.on( 'click', function( event ) {
+
+				closeMenu();
+				return false;
+
+			} );
+
+			$details.on( 'click', function( event ) {
+
+				$container.removeClass( 'rm-in' ).children( 'div.cv-modal' ).remove();
+				viewDetails( $( this ) );
+				return false;
+
+			} );
+			
+		},
+		openMenu = function() {
+
+			$container.addClass( 'cv-open' );
+
+		},
+		flipMenu = function() {
+
+			$container.addClass( 'cv-flip' );
+
+		},
+
+    unflipMenu = function() {
+
+			$container.removeClass( 'cv-flip' );
+
+		},
+
+		closeMenu = function() {
+
+			$container.removeClass( 'cv-open cv-nodelay cv-in cv-close' );
+
+		},
+		viewDetails = function( recipe ) {
+
+			var title = recipe.text(),
+				img = recipe.data( 'thumb' ),
+				description = recipe.parent().next().text(),
+				url = recipe.attr( 'href' );
+
+			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><a href="' + url + '">See the recipe</a><span class="rm-close-modal">x</span></div>' );
+
+			$modal.appendTo( $container );
+
+			var h = $modal.outerHeight( true );
+			$modal.css( 'margin-top', -h / 2 );
+
+			setTimeout( function() {
+
+				$container.addClass( 'rm-in rm-nodelay' );
+
+				$modal.find( 'span.cv-close-modal' ).on( 'click', function() {
+
+					$container.removeClass( 'rm-in' );
+
+				} );
+			
+			}, 0 );
+
+		};
+
+	return { init : init };
+
+})();
