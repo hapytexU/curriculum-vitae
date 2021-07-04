a83ab318e5119d98fdd58f77eccc31f73e00cfdd
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-07-04 10:35:20 +0200

    update the cv periodically

--- count_answers.sh
+++ count_answers.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 qst=$(curl 'https://api.stackexchange.com/2.2/users/67579/answers?order=desc&sort=activity&site=stackoverflow&filter=total' -q 2>/dev/null --output - | gunzip | jq -r '.total')
+echo $qst
