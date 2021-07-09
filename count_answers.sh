1cda4eb4fda12eb6e24b00c7080cfa4c8d3c5763
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-07-09 15:58:35 +0200

    use US locale

--- count_answers.sh
+++ count_answers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LC_NUMERIC="en_US.UTF-8"
 qst=$(curl 'https://api.stackexchange.com/2.2/users/67579/answers?order=desc&sort=activity&site=stackoverflow&filter=total' -q 2>/dev/null --output - | gunzip | jq -r '.total')
 qst=$(numfmt --grouping "$qst")
 sed -E "s/[?]{8}/$qst/" site/index.html
