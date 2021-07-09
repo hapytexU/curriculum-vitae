5c106cf144acd8256e2fc34128a7af79b27c1c6f
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-07-09 15:53:03 +0200

    add locale

--- count_answers.sh
+++ count_answers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LC_NUMERIC=en_US
 qst=$(curl 'https://api.stackexchange.com/2.2/users/67579/answers?order=desc&sort=activity&site=stackoverflow&filter=total' -q 2>/dev/null --output - | gunzip | jq -r '.total')
 qst=$(printf "%'.f\n" "$qst")
 sed -E "s/[?]{8}/$qst/" site/index.html
